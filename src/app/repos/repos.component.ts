887c9efb3e7ec416be71f63980993e5a39919a51
Jay-68 <kelvinngari1995@gmail.com>
2019-09-30 14:58:36 +0300

    changes to the readme file

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../display-profile/users.service';
 import { HttpClient } from '@angular/common/http';
 import { Repositories } from '../repositories';
-import {Profiles} from '../profiles';
+import { Profiles } from '../profiles';
 
 
 @Component({
@@ -16,31 +16,33 @@ export class ReposComponent implements OnInit {
   username: string = "";
   repositories: Repositories[];
   profiles: Profiles[];
-  users: any=[];
+  users: any = [];
 
-  loading: boolean=false;
+  loading: boolean = false;
   errorMessage;
 
 
-  constructor(private usersService:UsersService, private http:HttpClient ) { }
-     public getRepositories(){
-      this.loading=true;
-      this.errorMessage=" ";
-      this.usersService.getRepositories(this.username)
-        .subscribe((response) => {this.repositories=response;
+  constructor(private usersService: UsersService, private http: HttpClient) { }
+  public getRepositories() {
+    this.loading = true;
+    this.errorMessage = " ";
+    this.usersService.getRepositories(this.username)
+      .subscribe((response) => {
+      this.repositories = response;
         console.log(response)
-        },
-        (error) => {this.errorMessage=error; this.loading=false; },
-        () => {this.loading=false})
-      }
-      
-      public getProfiles(event:any){
-        
-        this.usersService.getProfiles(this.username)
-          .subscribe((response) => {this.users=response;
-          console.log(response)
-          })
-        }
+      },
+        (error) => { this.errorMessage = error; this.loading = false; },
+        () => { this.loading = false })
+  }
+
+  public getProfiles(event: any) {
+
+    this.usersService.getProfiles(this.username)
+      .subscribe((response) => {
+      this.users = response;
+        console.log(response)
+      })
+  }
 
   ngOnInit() {
   }
