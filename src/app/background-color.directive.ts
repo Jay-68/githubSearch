bc9aa90a96ceffe0afd396c2894dcce4272e39c6
Jay-68 <kelvinngari1995@gmail.com>
2019-09-30 14:37:44 +0300

    Add styles to the repository section

--- src/app/background-color.directive.ts
+++ src/app/background-color.directive.ts
@@ -6,6 +6,6 @@ import { Directive, ElementRef } from '@angular/core';
 export class BackgroundColorDirective {
 
   constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.backgroundColor = 'black';
+    this.elem.nativeElement.style.backgroundColor = 'grey';
   }
 }
